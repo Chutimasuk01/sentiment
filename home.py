aa3c9d473b6cc833a262bff6108f17055cef4191
Chutimasuk01 <chutima18092544@gmail.com>
2023-10-10 12:20:16 +0700

    .commit

--- home.py
+++ home.py
@@ -16,7 +16,3 @@ st.header("การวิเคราะห์ความรู้สึกภ
 st.subheader("Chutima Suksamai")
 st.image('./image/001.jpg')
 
-lot3="https://lottie.host/3d462873-9b5f-4269-88e3-aec72bd823b3/nsnU9gsvXR.json"
-lottie3 = load_lottieurl(lot3)
-st_lottie(lottie3)
-st.balloons()
