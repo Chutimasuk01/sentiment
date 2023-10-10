a3ba82d6460bc84a1654bf633e21efecadc54e16
Chutimasuk01 <chutima18092544@gmail.com>
2023-10-10 12:38:38 +0700

    .commit

--- home.py
+++ home.py
@@ -9,7 +9,7 @@ col1,col2 =st.columns(2)
 with col1:
     st.image('./image/001.jpg')
 with col2:
-    st.markdown("<center>Chutima Suksamai<br>สาขาวิชาวิทยาการข้อมูล NPRU</center>",unsafe_allow_html = True)
+    st.markdown(f'<h1 style="color:#33ff33;font-size:24px;">{"ColorMeBlue text”"} ชุติมา <br> สาขา</h1>', unsafe_allow_html=True)
     
 
 
