6dd0add78b475975e5be10ce50aba3d9facd3ab8
Chutimasuk01 <chutima18092544@gmail.com>
2023-10-10 11:29:51 +0700

    commit

--- home.py
+++ home.py
@@ -1,7 +1,6 @@
 import json
 import time
 import requests
-
 import streamlit as st
 from streamlit_lottie import st_lottie
 from streamlit_lottie import st_lottie_spinner
@@ -12,9 +11,10 @@ def load_lottieurl(url: str):
         return None
     return r.json()
 
+
 st.header = ("การวิเคราะห์ความรู้สึกภาษาไทย")
 st.subheader("ชุติมา")
-st.imge()
+st.imge("image")
 lot3="https://lottie.host/3d462873-9b5f-4269-88e3-aec72bd823b3/nsnU9gsvXR.json"
 lottie3 = load_lottieurl(lot3)
 st_lottie(lottie3)
