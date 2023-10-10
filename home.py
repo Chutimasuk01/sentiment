44d3c08b982efab9b1de4f79b48f38cde520c43a
Chutimasuk01 <chutima18092544@gmail.com>
2023-10-10 13:28:41 +0700

    .commit

--- home.py
+++ home.py
@@ -4,12 +4,12 @@ import requests
 import streamlit as st
 
 st.header("การวิเคราะห์ความรู้สึกภาษาไทย")
-
+st.subheader("ชุติมา สุขสมัย <br> สาขาวิทยาการข้อมูล<br>NPRU")
 col1,col2 =st.columns(2)
 with col1:
     st.image('./image/001.jpg')
 with col2:
-    st.markdown(f'<h1 style="color:#0F36BD;font-size:24px;">{"ชุติมา สุขสมัย<br> สาขาวิทยาการข้อมูล<br>NPRU"} </h1>', unsafe_allow_html=True)
+    st.image('./image/002.jpg')
     
 
 
