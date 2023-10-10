4e2f4e17e061ee4762bfdcf13bf9492ef60b43a0
Chutimasuk01 <chutima18092544@gmail.com>
2023-10-10 12:25:58 +0700

    .commit

--- home.py
+++ home.py
@@ -2,14 +2,7 @@ import json
 import time
 import requests
 import streamlit as st
-from streamlit_lottie import st_lottie
-from streamlit_lottie import st_lottie_spinner
 
-def load_lottieurl(url: str):
-    r = requests.get(url)
-    if r.status_code != 200:
-        return None
-    return r.json()
 
 
 st.header("การวิเคราะห์ความรู้สึกภาษาไทย")
