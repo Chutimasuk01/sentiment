f33418002a792a11988cdc80c8ae3cf33502d9b9
Chutimasuk01 <chutima18092544@gmail.com>
2023-10-10 12:32:03 +0700

    .commit

--- home.py
+++ home.py
@@ -4,12 +4,13 @@ import requests
 import streamlit as st
 
 st.header("การวิเคราะห์ความรู้สึกภาษาไทย")
-st.subheader("Chutima Suksamai")
+
 col1,col2 =st.columns(2)
 with col1:
-    st.subheader("สาขาวิชาวิทยาการข้อมูล NPRU")
-with col2:
     st.image('./image/001.jpg')
+with col2:
+    st.subheader("Chutima Suksamai")
+    st.subheader("สาขาวิชาวิทยาการข้อมูล NPRU")
 
 
 
